5fb5d6cacdc381520e8621bb450e2d8fc04a5538
Daniel Naylor <git@drnaylor.co.uk>
2019-04-21 23:47:23 +0100

    Licence blocks

--- src/main/java/io/github/nucleuspowered/nucleus/modules/core/registry/TeleportScannerRegistryModule.java
+++ src/main/java/io/github/nucleuspowered/nucleus/modules/core/registry/TeleportScannerRegistryModule.java
@@ -1,10 +1,16 @@
+/*
+ * This file is part of Nucleus, licensed under the MIT License (MIT). See the LICENSE.txt file
+ * at the root of this project for more details.
+ */
 package io.github.nucleuspowered.nucleus.modules.core.registry;
 
 import io.github.nucleuspowered.nucleus.internal.registry.NucleusRegistryModule;
 import io.github.nucleuspowered.nucleus.internal.teleport.scanners.AscendingTeleportScanner;
 import io.github.nucleuspowered.nucleus.internal.teleport.scanners.ITeleportScanner;
 import io.github.nucleuspowered.nucleus.internal.teleport.scanners.NoTeleportScanner;
+import org.spongepowered.plugin.meta.util.NonnullByDefault;
 
+@NonnullByDefault
 public class TeleportScannerRegistryModule extends NucleusRegistryModule<ITeleportScanner> {
 
     @Override
