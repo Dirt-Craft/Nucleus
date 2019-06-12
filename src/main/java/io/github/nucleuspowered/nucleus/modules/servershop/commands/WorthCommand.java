7b32abd39eb679965cf5668782a04415b8df0925
d4julian <julianubico@icloud.com>
2019-06-12 16:10:22 -0400

    typo

--- src/main/java/io/github/nucleuspowered/nucleus/modules/servershop/commands/WorthCommand.java
+++ src/main/java/io/github/nucleuspowered/nucleus/modules/servershop/commands/WorthCommand.java
@@ -78,7 +78,7 @@ public class WorthCommand extends AbstractCommand<CommandSource> {
                         "&6&l" + quantity + " &b" + player.getItemInHand(HandTypes.MAIN_HAND).get().getType().getName() + "&7's are worth &a$" + price));
             } else {
                 src.sendMessage(Util.format(
-                        "A &b" + player.getItemInHand(HandTypes.MAIN_HAND).get().getType().getName() + "&7 is worth &a$" + price));
+                        "&7A &b" + player.getItemInHand(HandTypes.MAIN_HAND).get().getType().getName() + "&7 is worth &a$" + price));
             }
         } else {
             src.sendMessage(Util.format(
